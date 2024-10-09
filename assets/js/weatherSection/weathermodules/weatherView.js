947d8c715723f3e2a87d50e7e80195a0756eae6f
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-10-09 14:45:26 +0200

    Styling(not done)

--- assets/js/weatherSection/weathermodules/weatherView.js
+++ assets/js/weatherSection/weathermodules/weatherView.js
@@ -1,5 +1,20 @@
-export function weatherView(data) {
+export function weatherView(data, date) {
     const section = document.getElementById('vejr-info');
+    section.innerHTML = '';
+    
+
+    const weatherDiv = document.createElement('div');
+    const weekday = document.createElement('p');
+    const day = document.createElement('p');
+    const month = document.createElement('p');
+    weekday.textContent = date.weekday;
+    day.textContent = date.day;
+    month.textContent = date.month;
+    section.appendChild(weatherDiv);
+    weatherDiv.appendChild(weekday);
+    weatherDiv.appendChild(day);
+    weatherDiv.appendChild(month);
+
     const div = document.createElement('div');
     const icon = document.createElement('img');
     icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
@@ -9,7 +24,6 @@ export function weatherView(data) {
     temp.textContent = `${tempText}°C`;
 
     section.appendChild(div);
-    section.innerHTML = '';
     div.appendChild(icon);
     div.appendChild(temp);
 
